387691f429b1438bdf93bd8406dd89f184c278e4
Andrea Prat <andr8ide@gmail.com>
2023-08-18 19:17:40 +0200

    Test loadUsers slice and create usersMock

---
+++ src/store/users/__tests__/loadUsers.test.ts
@@ -0,0 +1,20 @@
+import { usersMock } from "../../../components/mocks/usersMock";
+import { User } from "../../types";
+import { UsersState } from "../types";
+import { loadUsersActionCreator, usersReducer } from "../usersSlice";
+
+describe("Given a loadUsers reducer", () => {
+  describe("When it should receives a state and loadUser action with three users 'catwoman', 'jocker' and 'harley quinn'", () => {
+    test("Then it should return a new state with three users 'catwoman', 'jocker' and 'harley quinn'", () => {
+      const currentUserState: UsersState = {
+        users: [],
+      };
+      const users: User[] = usersMock;
+
+      const loadUserAction = loadUsersActionCreator(users);
+      const newUserState = usersReducer(currentUserState, loadUserAction);
+
+      expect(newUserState).toHaveProperty("users", usersMock);
+    });
+  });
+});
