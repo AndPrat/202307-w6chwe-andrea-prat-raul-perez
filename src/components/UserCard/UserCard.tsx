52d8a6801f8831b82a1cf144cfdcf2bd6437a95d
Andrea Prat <andr8ide@gmail.com>
2023-08-19 13:33:38 +0200

    Add in the UserCard the properties of the Api

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -5,14 +5,32 @@ export interface UserCardProps {
 }
 
 const UserCard = ({
-  user: { name, age, isFriend },
+  user: {
+    name,
+    age,
+    zodiacSign,
+    favouriteAnimal,
+    bestFriend,
+    isFriend,
+    picture,
+  },
 }: UserCardProps): React.ReactElement => {
   return (
     <article className="users-card">
+      <img
+        src={picture}
+        alt={`${name}'s user ilustration`}
+        width="20"
+        height="26"
+        className="user__picture"
+      />
       <h2>{name}</h2>
       <ul>
-        <li>{age}</li>
         <li>{isFriend}</li>
+        <li>{age}</li>
+        <li>{zodiacSign}</li>
+        <li>{favouriteAnimal}</li>
+        <li>{bestFriend}</li>
       </ul>
     </article>
   );
