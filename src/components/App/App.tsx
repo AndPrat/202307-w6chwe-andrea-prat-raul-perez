67b99f6c0eedac6bf87ab01975b58b9db773e244
Raúl Pérez <jrpcassone@gmail.com>
2023-08-20 12:25:01 +0200

    Implement error handling in useUsersApi and App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,16 +3,19 @@ import UsersListPage from "../../pages/UsersListPage/UsersListPage";
 import paths from "../../paths/paths";
 import { useAppSelector } from "../../store";
 import Counter from "../Counter/Counter";
+import Error from "../Error/Error";
 import Header from "../Header/Header";
 import Loader from "../Loader/Loader";
 import "./App.css";
 
 const App = (): React.ReactElement => {
   const isLoading = useAppSelector((state) => state.uiStore.isLoading);
+  const isError = useAppSelector((state) => state.uiStore.isError);
 
   return (
     <>
       {isLoading && <Loader />}
+      {isError && <Error />}
       <div className="container">
         <Header />
         <main className="main-content">
